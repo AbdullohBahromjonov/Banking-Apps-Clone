5463203e51ac7ebd7fdab0ce71db013afe96fb4c
Abdulloh <abdullohbahromjonv@gmail.com>
2024-01-23 19:47:18 +0500

    Initial Commit

---
+++ Banks clone/Banks_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banks_cloneApp.swift
+//  Banks clone
+//
+//  Created by Abdulloh on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Banks_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
